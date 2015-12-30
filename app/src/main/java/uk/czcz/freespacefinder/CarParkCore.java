02cfecdda366abf4ffc8d42b2184171dece224f4
Chris Sawczuk <gummybassist@gmail.com>
2015-12-30 17:05:51 +0000

    Starting to flesh out CarParkCore, can now fetch pages of a car park list

--- app/src/main/java/uk/czcz/freespacefinder/CarParkCore.java
+++ app/src/main/java/uk/czcz/freespacefinder/CarParkCore.java
@@ -1,13 +1,43 @@
 package uk.czcz.freespacefinder;
 
+import java.util.List;
+
 public class CarParkCore {
+
+    public interface CarParkFetcher
+    {
+        interface Callback
+        {
+            void noCarParksRetreieved();
+            void carParksRetrieved(List<CarPark> carParks);
+        }
+
+        void fetch(int pageNumber, Callback callback);
+    }
 
     public interface CarParkFetchCallback {
 
         void noCarParksRetrieved();
+        void carParksFetched(List<CarPark> carParks);
     }
 
-    public void fetchCarParks(CarParkFetchCallback fetchCallback) {
+    private final CarParkFetcher carParkFetcher;
 
+    public CarParkCore(CarParkFetcher carParkFetcher) {
+        this.carParkFetcher = carParkFetcher;
+    }
+
+    public void fetchCarParks(int pageNumber, final CarParkFetchCallback fetchCallback) {
+        carParkFetcher.fetch(pageNumber, new CarParkFetcher.Callback() {
+            @Override
+            public void noCarParksRetreieved() {
+                fetchCallback.noCarParksRetrieved();
+            }
+
+            @Override
+            public void carParksRetrieved(List<CarPark> carParks) {
+                fetchCallback.carParksFetched(carParks);
+            }
+        });
     }
 }
--- app/src/test/java/uk/czcz/freespacefinder/CarParkCoreTest.java
+++ app/src/test/java/uk/czcz/freespacefinder/CarParkCoreTest.java
@@ -2,6 +2,10 @@ package uk.czcz.freespacefinder;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -10,24 +14,74 @@ public class CarParkCoreTest {
     private static class SpyCarParkFetchCallback implements CarParkCore.CarParkFetchCallback {
 
         public boolean noCarParksRetrieved_called;
+        public List<CarPark> carParksFetched_called;
 
         @Override
         public void noCarParksRetrieved()
         {
             noCarParksRetrieved_called = true;
         }
+
+        @Override
+        public void carParksFetched(List<CarPark> carParks) {
+            carParksFetched_called = carParks;
+        }
+    }
+
+    private class NoCarParkFetcher implements CarParkCore.CarParkFetcher {
+
+        @Override
+        public void fetch(int pageNumber, Callback callback) {
+            callback.noCarParksRetreieved();
+        }
+    }
+
+    private class FakeCarParkFetcher implements CarParkCore.CarParkFetcher {
+        private List<CarPark>[] carParksToRetrievePages;
+
+        public FakeCarParkFetcher(List<CarPark> ...carParkList)
+        {
+            carParksToRetrievePages = carParkList;
+        }
+
+        @Override
+        public void fetch(int pageNumber, Callback callback) {
+            callback.carParksRetrieved(carParksToRetrievePages[pageNumber]);
+        }
     }
 
     @Test
     public void theCarParkCoreWillNotifyACallbackObjectWhenNoCarParksWereRetrieved()
     {
-        CarParkCore carParkCore = new CarParkCore();
+        CarParkCore carParkCore = new CarParkCore(new NoCarParkFetcher());
 
         SpyCarParkFetchCallback spyCarParkFetchCallback = new SpyCarParkFetchCallback();
-        carParkCore.fetchCarParks(spyCarParkFetchCallback);
+        carParkCore.fetchCarParks(0, spyCarParkFetchCallback);
 
         assertThat(spyCarParkFetchCallback.noCarParksRetrieved_called, is(true));
     }
 
+    @Test
+    public void theCarParkCoreWillNotifyACallbackWhenCarParksAreRetrieved() throws LastUpdateTimestamp.LastUpdateTimestampException {
+        CarPark expectedCarPark = new CarPark(1, "Name", new LastUpdateTimestamp("2015-12-30T15:12:15"), 53, -2, 10, 5, 2, 1);
+        CarParkCore carParkCore = new CarParkCore(new FakeCarParkFetcher(Arrays.asList(expectedCarPark)));
 
+        SpyCarParkFetchCallback spyCarParkFetchCallback = new SpyCarParkFetchCallback();
+        carParkCore.fetchCarParks(0, spyCarParkFetchCallback);
+
+        assertThat(spyCarParkFetchCallback.carParksFetched_called, hasItem(expectedCarPark));
+    }
+
+    @Test
+    public void theCarParkCoreWillNotifyACallbackWhenCarParksAreRetrievedForDifferentPages() throws LastUpdateTimestamp.LastUpdateTimestampException {
+        CarPark expectedCarPark = new CarPark(1, "Name", new LastUpdateTimestamp("2015-12-30T15:12:15"), 53, -2, 10, 5, 2, 1);
+        CarPark expectedCarParkTwo = new CarPark(2, "Name", new LastUpdateTimestamp("2015-12-30T15:12:15"), 53, -2, 10, 5, 2, 1);
+        CarParkCore carParkCore = new CarParkCore(new FakeCarParkFetcher(Arrays.asList(expectedCarPark), Arrays.asList(expectedCarParkTwo)));
+
+        SpyCarParkFetchCallback spyCarParkFetchCallback = new SpyCarParkFetchCallback();
+        carParkCore.fetchCarParks(0, spyCarParkFetchCallback);
+        assertThat(spyCarParkFetchCallback.carParksFetched_called, hasItem(expectedCarPark));
+        carParkCore.fetchCarParks(1, spyCarParkFetchCallback);
+        assertThat(spyCarParkFetchCallback.carParksFetched_called, hasItem(expectedCarParkTwo));
+    }
 }
